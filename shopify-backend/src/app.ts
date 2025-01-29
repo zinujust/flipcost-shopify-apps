fa70fc74acf567755e534f4d7be34f2e2962e0e7
Zainal <shariff.zainal7@gmail.com>
2025-01-29 11:45:27 -0800

    cleaned project directory

--- shopify-backend/src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-const app = express();
-app.use(express.json());
-
-export default app;
--- shopify-backend/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app"; // Ensure the correct path to the app module
-
-const port = process.env.PORT ?? 3000;
-
-app.listen(port, () => {
-  console.log("Server is running on port: ", port);
-});
