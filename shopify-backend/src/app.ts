d5ccbef1bf72f4ac97251a5eb822036e3631196f
Zainal <shariff.zainal7@gmail.com>
2025-02-16 01:23:06 -0800

    added corsOption to backend

--- shopify-backend/src/app.ts
+++ shopify-backend/src/app.ts
@@ -2,6 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/shopifyProductsRoutes";
 import cors from "cors";
+import { METHODS } from "http";
+
+const corsOptions = {
+  origin: "https://flipcost-shopify-apps.vercel.app",
+  METHODS: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true,
+};
 
 dotenv.config();
 const app = express();
